93d2525162a92c87332aad3f8a9d317eca1a7f60
Quang Minh Nguyen <quangng711@outlook.com.au>
2024-04-22 11:35:55 +1000

    Initial Commit

---
+++ BlissBubble/BlissBubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlissBubbleApp.swift
+//  BlissBubble
+//
+//  Created by Quang Minh Nguyen on 22/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct BlissBubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlissBubble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlissBubble
+//
+//  Created by Quang Minh Nguyen on 22/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
