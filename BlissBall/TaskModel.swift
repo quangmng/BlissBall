4eb96f8e2301b06212fd5028a91d4c30212f5e99
Wendy <lovelyalexzhou@outlook.com>
2024-04-26 18:43:24 +1000

    new changes!!

---
+++ BlissBall/TaskModel.swift
@@ -0,0 +1,8 @@
+//
+//  TaskModel.swift
+//  BlissBubble
+//
+//  Created by Wende Zhou on 26/4/2024.
+//
+
+import Foundation
---
+++ BlissBall/TaskView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskView.swift
+//  BlissBubble
+//
+//  Created by Wende Zhou on 26/4/2024.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskView()
+}
