ac4617610d6ef09d357b9e5f114bcd0818d794de
Quang Minh Nguyen <quangng711@outlook.com.au>
2024-05-06 10:44:28 +1000

    adding some shennanigans/hints for upcoming features

--- BlissBall/Feature/Tabs/BlissBall/BlissBallView.swift
+++ BlissBall/Feature/Tabs/BlissBall/BlissBallView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct BlissBallView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            Text("*Ball goes in this screen*")
+            Text("Shake phone or swipe across ball to activate")
+                .font(.title)
+            .navigationTitle("BlissBall")
+        }
     }
 }
 
--- BlissBall/Feature/Tabs/Setting/SettingView.swift
+++ BlissBall/Feature/Tabs/Setting/SettingView.swift
@@ -13,6 +13,7 @@ struct SettingView: View {
     @State var isHapticOn: Bool = false
     @State var isNotifOn: Bool = false
     @State var isHealthDataOn: Bool = false
+    @State var aboutSheet: Bool = false
     var body: some View {
         NavigationStack{
             List{
@@ -37,6 +38,12 @@ struct SettingView: View {
                     }
                 }
                 HStack{
+                    //About screen shows as a sheet (App name, icon, version number e.g. 1.0, Made with support of Inter8ctors and our iOS team*)
+                    Text("About BlissBall")
+                        .foregroundStyle(.blue)
+                }
+                HStack{
+                    //.alert "are you sure to clear out all your tasks and self-affirmations?" - option 1 Delete - option 2 Cancel
                     Text("Clear All User Entries")
                         .foregroundStyle(.red)
                 }
