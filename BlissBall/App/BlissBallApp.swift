829c23f61bf69494bd5be0e37b091822ddbc5045
Quang Minh Nguyen <quangng711@outlook.com.au>
2024-05-13 13:25:31 +1000

    PushNotif Testing

--- BlissBall/App/BlissBallApp.swift
+++ BlissBall/App/BlissBallApp.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 @main
 struct BlissBallApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+// MARK: - AppDelegate
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
+    registerForPushNotifications()
+    return true
+  }
+  
+  // MARK: - Notification Functions
+  
+  func registerForPushNotifications() {
+    UNUserNotificationCenter.current()
+      .requestAuthorization(options: [.alert, .sound, .badge]) {(granted, error) in
+        print("Permission granted: \(granted)")
+      }
+  }
+  
 }
