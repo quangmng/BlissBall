4af670f1dfd3940f22fe2a986a242189498521f8
Quang Minh Nguyen <quangng711@outlook.com.au>
2024-05-13 10:12:36 +1000

    attempting to hide tabView in TaskView

---
+++ BlissBall/Feature/Tabs/TabViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewModel.swift
+//  BlissBubble
+//
+//  Created by Quang Minh Nguyen on 13/5/2024.
+//
+
+import SwiftUI
+
+struct TabViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabViewModel()
+}
